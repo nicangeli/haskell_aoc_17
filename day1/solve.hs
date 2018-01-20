f6bb892291138f57f9ec1a9ac541c445378174bb
Nick Angeli <nicangeli@gmail.com>
2018-01-20 09:55:22 +0000

    First attempt at Haskell AOC Day1

--- day1/solve.hs
+++ day1/solve.hs
@@ -8,6 +8,7 @@ main :: IO ()
 main = do
   input <- readFile "input.txt"
   print $ part1 input
+  print $ part2 input
 
 part1 :: String -> Int
 part1 = sum_valid_pairs . build_pairs
@@ -22,3 +23,9 @@ is_valid_pair :: (Char, Char) -> Bool
 is_valid_pair (x,y)
   | x == y = True
   | otherwise = False
+
+part2 :: String -> Int
+part2 = sum_valid_pairs . build_pivot_pairs
+
+build_pivot_pairs :: String -> [(Char, Char)]
+build_pivot_pairs digits = zip digits (take (length digits) $ drop (length digits `div` 2) $ cycle digits)
